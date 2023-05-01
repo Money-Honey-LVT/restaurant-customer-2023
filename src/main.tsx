a6411c0ed58c47c619874f8e870162add54516f6
thanhtran17 <enigma15012001@gmail.com>
2023-05-01 12:51:55 +0700

    apply base

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { MantineProvider } from '@mantine/core';
+import { ModalsProvider } from '@mantine/modals';
+import { Notifications } from '@mantine/notifications';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from 'react';
+import { CookiesProvider } from 'react-cookie';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import AppRoutes from './pages/routers';
+import store from './redux/store';
+import customTheme from './theme';
+
+function App() {
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <CookiesProvider>
+          <BrowserRouter>
+            <MantineProvider theme={customTheme}>
+              <ModalsProvider>
+                <Notifications position="top-right" />
+                <AppRoutes />
+              </ModalsProvider>
+            </MantineProvider>
+          </BrowserRouter>
+        </CookiesProvider>
+      </Provider>
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
